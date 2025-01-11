43ac876474d4c105eebe31cac70e0e54dde84eb2
Alejandro Malvacias <alejandromalvacias1@gmail.com>
2025-01-11 12:10:26 -0400

    Finish the project

---
+++ scripts/menu.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburgerMenu = document.querySelector('.hamburger-menu');
+    const nav = document.querySelector('nav');
+
+    hamburgerMenu.addEventListener('click', () => {
+        nav.classList.toggle('active');
+    });
+});
